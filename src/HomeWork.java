8835dd8b7f227d10cc3841ee6b1c34642ba8a6cf
demidovka <demidov.kirill.1992@gmail.com>
2019-01-16 19:50:06 +0300

    1

---
+++ src/HomeWork.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // подключаем сканер
+public class HomeWork {
+        public static void main (String [] args) {
+
+            System.out.print ("Введите число 1, 2 или 3: ");
+            Scanner inputFigure = new Scanner (System.in);
+            int i = inputFigure.nextInt ();
+            if ( i==1) {
+                System.out.println ("Вы ввели число 1");
+            }
+            else if (i==2) {
+                System.out.println ("Вы ввели число 2");
+            }
+            else if (i==3) {
+                System.out.println ("Вы ввели число 3");
+            }
+            else {
+                System.out.println ("Вы ввели число не равное 1, 2 или 3");
+            }
+        }
+    }
